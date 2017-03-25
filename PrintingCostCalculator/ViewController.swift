bf64d8065575d7e89b6b0c276cd77ec76b915a11
Sven Forstner <sforstner700@gmail.com>
2017-03-25 21:36:39 +0100

    Initial Commit

---
+++ PrintingCostCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PrintingCostCalculator
+//
+//  Created by Sven Forstner on 25.03.17.
+//  Copyright © 2017 Sven Forstner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
